81b897303c124cd6c131188b28074b462f269c30
xelav <xelav.rus@gmail.com>
2018-08-12 10:28:43 +0300

    Rename OfficeAppApplication -> OfficeApp

--- src/main/java/org/xelav/officeapp/OfficeApp.java
+++ src/main/java/org/xelav/officeapp/OfficeApp.java
@@ -2,14 +2,13 @@ package org.xelav.officeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.xelav.officeapp.dao")
-public class OfficeAppApplication {
+public class OfficeApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(OfficeAppApplication.class, args);
+        SpringApplication.run(OfficeApp.class, args);
     }
 }
